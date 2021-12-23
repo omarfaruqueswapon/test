f78c9e010304ec51cb8e2d806b0fbc9799edde55
omar <omarfaruqueswapon@gmail.com>
2021-12-23 16:39:50 +0600

    1 file added

--- nitika whats in the name.py
+++ nitika whats in the name.py
@@ -3,13 +3,5 @@ names=[]
 for tc in range(T):
 	# Read integers a and b.
 	names.append(list(input().split(" ")))
-
-for i in range(len(names)):
-	if  len(names[i][0])>1 and len(names[i])==1:
-		print(names[i][0])
-	if len(names[i][0])>1:
-		print(names[i][0][0].capitalize()+".")
-	if len(names[i][1])>1:
-		print(names[i][1][0].capitalize()+".")
 		
 		
