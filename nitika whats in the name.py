77f68fe905d4b2433ef3bcc8581e97cfce277a61
omar <omarfaruqueswapon@gmail.com>
2021-12-23 17:17:37 +0600

    partly done

--- nitika whats in the name.py
+++ nitika whats in the name.py
@@ -3,5 +3,16 @@ names=[]
 for tc in range(T):
 	# Read integers a and b.
 	names.append(list(input().split(" ")))
+
+for i in range(len(names)):
+	if  len(names[i])==1:
+		continue
+	if  len(names[i])==2:
+		names[i][0]=names[i][0][0].capitalize()+"."
+		print(names[i])
+	if  len(names[i])==3:
+		names[i][0]=names[i][0][0].capitalize()+"."
+		names[i][1]=names[i][1][0].capitalize()+"."
+		print(names[i])
 		
-		
+print(names)
--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
 arr=['hello','dfsdf']
 print(arr)
-arr[0]=[4]
+arr[0]=arr[0].capitalize()
 print(arr)
