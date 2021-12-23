e7e2083dd708524dfe6bd4228c44471b912a6d7e
omar <omarfaruqueswapon@gmail.com>
2021-12-23 17:03:50 +0600

    partly done

---
+++ test.py
@@ -0,0 +1,18 @@
+T = int(input())
+names=[]
+for tc in range(T):
+	# Read integers a and b.
+	names.append(list(input().split(" ")))
+
+for i in range(len(names)):
+	if  len(names[i])==1:
+		print(names[i][0])
+	if  len(names[i])==2:
+		print(names[i][0][0].capitalize()+".")
+	if  len(names[i])==3:
+		print(names[i][1][0].capitalize()+".")
+	# if len(names[i][0])>1:
+	# 	print(names[i][0][0].capitalize()+".")
+	# if len(names[i][1])>1:
+	# 	print(names[i][1][0].capitalize()+".")
+		
---
+++ test2.py
@@ -0,0 +1,4 @@
+arr=['hello','dfsdf']
+print(arr)
+arr[0]=[4]
+print(arr)
